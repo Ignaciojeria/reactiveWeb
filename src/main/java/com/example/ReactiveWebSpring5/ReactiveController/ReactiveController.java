27613a4c7d97dae6c55b4bc1dd8a675338764336
ignacio <Ignaciovl.j@gmail.com>
2017-10-26 19:55:24 -0300

    ReactiveWebController

---
+++ src/main/java/com/example/ReactiveWebSpring5/ReactiveController/ReactiveController.java
@@ -0,0 +1,27 @@
+package com.example.ReactiveWebSpring5.ReactiveController;
+
+import java.time.Duration;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ReactiveController {
+
+	public static void multiplicar (Integer n) {
+		System.out.println(n *2);
+	}
+	
+	 @GetMapping(path = "/numeros", produces = "text/event-stream")
+	    public Flux<Integer> all () {
+	        Flux<Integer> flux = Flux.range(1,30)
+	                .delayElements(Duration.ofSeconds(1))
+	                .filter(n-> n%2==0) // solo números divisibles entre 2
+	                .map(n -> n*2); // a cada elemento que ha sido filtrado, lo multiplicamos por 2
+	        flux.subscribe(System.out::println); // suscriptor 1
+	        flux.subscribe(ReactiveController::multiplicar); // suscriptor 2
+	        return flux; // retornamos el elemento. Sería como el suscriptor 3
+	    }
+}
---
+++ src/main/java/com/example/ReactiveWebSpring5/ReactiveWebSpring5Application.java
@@ -0,0 +1,12 @@
+package com.example.ReactiveWebSpring5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveWebSpring5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveWebSpring5Application.class, args);
+	}
+}
